5fc6e664e71f7636f3e1c28d1e7b4066001f060f
AffraAdiWirawan <Affrawirawan9@gmail.com>
2023-03-16 22:53:14 +0700

    pengembalian fungsi input

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,3 +5,25 @@ using namespace std;
 int a[20];
 int a;
 
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada Array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+}
+cout << endl;
+cout << "====================" << endl;
+cout << "Masukkan Elemen Array" << endl;
+cout << "====================" << endl;
+
+for (int i = 1); i < n; i++) {
+	cout << "Data ke-" << (i + 1) << ": ";
+	cin >> a[i];
+
+
+}
