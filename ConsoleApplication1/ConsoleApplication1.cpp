6effa9088e9713a0026e2148c97282b89b4ef02c
AffraAdiWirawan <Affrawirawan9@gmail.com>
2023-03-16 23:37:02 +0700

    pembuatan main program

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 
 int a[20];
-int a;
+int n;
 
 void input() {
 	while (true) {
-		cout << "Masukkan banyaknya elemen pada Array : ";
+		cout << " Masukkan banyaknya elemen pada array :";
 		cin >> n;
 		if (n <= 20)
 			break;
@@ -15,21 +15,21 @@ void input() {
 			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
 		}
 	}
-}
-cout << endl;
-cout << "====================" << endl;
-cout << "Masukkan Elemen Array" << endl;
-cout << "====================" << endl;
-
-for (int i = 1); i < n; i++) {
-	cout << "Data ke-" << (i + 1) << ": ";
-	cin >> a[i];
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+	
+	for (int i = 0; i < n; i++) {
+		cout << " Data  ke-" << (i + 1) << ":";
+		cin >> a[i];
+	}
 }
 void bubbleSortArray() {
-	for (int i = 1); i < n; i++){
-		for (int j = 0; < n - i; j++) {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - 1; j++){
 			if (a[j] > a[j + 1]) {
-				int temp + a[j];
+				int temp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = temp;
 			}
